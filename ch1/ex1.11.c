0926a3d1faf769a35a85fb92ba8d58207ddeb98f
michalbr <82528394+michalbr@users.noreply.github.com>
2026-07-15 16:02:02 +0000

    Add Chapter 1 exercise 1-11 implementation in ch1/ex1.11.c

---
+++ ch1/ex1.11.c
@@ -0,0 +1,8 @@
+/*
+Q: How would you test the word count program? What kinds of input are most likely to uncover bugs if there are any?
+
+My guess is using other whitespace characters than newline, space, or tab. Those will be considered as characters, and counted towards word characters.
+
+One word (single character), empty input and single space worked ok.
+
+*/
