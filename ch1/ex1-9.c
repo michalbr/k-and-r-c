c844cae5cf0823ba4439dcdaf8391ec183bb6a9e
Michal B <mikebee661@gmail.com>
2026-07-15 15:59:54 +0200

    Updated ex1-9 added ex1-10 from ch1

--- ch1/ex1-9.c
+++ ch1/ex1-9.c
@@ -2,29 +2,14 @@
 
 int main(void)
 {
-    int c, blanks, tabs, newlines;
+    int c, prev;
 
-    newlines = 0;
-    tabs = 0;
-    blanks = 0;
+    prev = 0;
     while ((c = getchar()) != EOF)
     {
-        if (c == '\n')
-            ++newlines;
-        else if (c == '\t')
-            ++tabs;
-        else if (c == ' ')
-            ++blanks;
+        if (c != ' ' || prev != ' ')
+            putchar(c);
+
+        prev = c;
     }
 }
-
-
-/*
-- declare prev variable to int
-- initialize prev to 0
-- get a character and save it into variable c
-    - if prev does not equal blank
-        - print curernt character
-    - save current character to prev
-    - this is test line for commit
-*/
